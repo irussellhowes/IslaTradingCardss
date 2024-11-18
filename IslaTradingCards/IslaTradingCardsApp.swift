33fefe12148c268e4b43fee56c13c6c48a04dfb2
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2024-11-18 17:45:49 -0500

    Initial Commit

---
+++ IslaTradingCards/IslaTradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslaTradingCardsApp.swift
+//  IslaTradingCards
+//
+//  Created by Isla Russell-Howes on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct IslaTradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
