00870fec9fc57201b179e2dd3be527739ab5846b
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2024-11-18 23:26:39 -0500

    Changed spelling. Applied abstraction to the rest of the detials structure.

--- IslaTradingCards/ViewDetails.swift
+++ IslaTradingCards/ViewDetails.swift
@@ -20,7 +20,7 @@ let laybackInaBauer = TradingCard(
     notes: "Extended 4th pos."
 )
 
-let beillmannSpin = TradingCard(
+let biellmannSpin = TradingCard(
     imageName: "BeillmannSpin",
     elementName: "BeillmannSpin",
     type: "Upright Spin",
@@ -32,92 +32,86 @@ let beillmannSpin = TradingCard(
 
 struct ViewDetails: View {
     
-    let tradingCard: TradingCard
+    let TradingCard: TradingCard
     var body: some View {
         ZStack {
-            Image(tradingCard.imageName)
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
+            Image(
+                "snow"
+            )
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(
+                .all
+            )
             VStack {
                 ZStack {
                     Rectangle()
-                        .fill(Color.teal)
-                        .padding(.all,25)
-                    
-                    Rectangle()
-                        .fill(Color.white)
-                        .padding(.all,30)
-                    
-                    Rectangle()
-                        .fill(Color.teal)
-                        .padding(.all,35)
-                    
-                    Image("InaBauer")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 475)
-                }
-                .frame(height:555)
-                
-                Text("Layback Ina Bauer")
-                    .font(
-                        
-                        .custom(
-                            "Palatino",
-                            size: 36.0,
-                            relativeTo: .body
+                        .fill(
+                            Color.teal
                         )
+                        .padding(
+                            .all,
+                            25
+                        )
+                    
+                    Rectangle()
+                        .fill(
+                            Color.white
+                        )
+                        .padding(
+                            .all,
+                            30
+                        )
+                    
+                    Rectangle()
+                        .fill(
+                            Color.teal
+                        )
+                        .padding(
+                            .all,
+                            35
+                        )
+                    
+                    Image(
+                        TradingCard.imageName
                     )
-                    .foregroundColor(.teal)
-                    .padding(.bottom,10)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(
+                        height: 475
+                    )
+                }
+                .frame(
+                    height:555
+                )
+                
+                Text(
+                    TradingCard.elementName
+                )
+                .font(
+                    
+                    .custom(
+                        "Palatino",
+                        size: 36.0,
+                        relativeTo: .body
+                    )
+                )
+                .foregroundColor(
+                    .teal
+                )
+                .padding(
+                    .bottom,
+                    10
+                )
                 
                 HStack{
-                    VStack(alignment: .center){
+                    VStack(
+                        alignment: .center
+                    ){
                         HStack{
-                            Text("Type:")
-                                .font(
-                                    .custom(
-                                        "Palatino",
-                                        size: 20.0,
-                                        relativeTo: .body
-                                    )
-                                )
-                                .foregroundColor(.teal)
-                            Text("MITF")
-                                .font(
-                                    .custom(
-                                        "Palatino",
-                                        size: 20.0,
-                                        relativeTo: .body
-                                    )
-                                )
-                                .foregroundColor(.teal)
-                        }
-                        .padding(.bottom,10)
-                        HStack{
-                            Text("Base Value:")
-                                .font(
-                                    .custom(
-                                        "Palatino",
-                                        size: 20.0,
-                                        relativeTo: .body
-                                    )
-                                )
-                                .foregroundColor(.teal)
-                            Text("N/A")
-                                .font(
-                                    .custom(
-                                        "Palatino",
-                                        size: 20.0,
-                                        relativeTo: .body
-                                    )
-                                )
-                                .foregroundColor(.teal)
-                        }
-                        .padding(.bottom,10)
-        
-                            Text("First Performer:")
+                            Text(
+                                "Type:"
+                            )
                             .font(
                                 .custom(
                                     "Palatino",
@@ -125,8 +119,12 @@ struct ViewDetails: View {
                                     relativeTo: .body
                                 )
                             )
-                            .foregroundColor(.teal)
-                            Text("Ina Bauer")
+                            .foregroundColor(
+                                .teal
+                            )
+                            Text(
+                                TradingCard.type
+                            )
                             .font(
                                 .custom(
                                     "Palatino",
@@ -134,53 +132,138 @@ struct ViewDetails: View {
                                     relativeTo: .body
                                 )
                             )
-                            .foregroundColor(.teal)
+                            .foregroundColor(
+                                .teal
+                            )
+                        }
+                        .padding(
+                            .bottom,
+                            10
+                        )
+                        HStack{
+                            Text(
+                                "Base Value:"
+                            )
+                            .font(
+                                .custom(
+                                    "Palatino",
+                                    size: 20.0,
+                                    relativeTo: .body
+                                )
+                            )
+                            .foregroundColor(
+                                .teal
+                            )
+                            Text(
+                                TradingCard.baseValue
+                            )
+                            .font(
+                                .custom(
+                                    "Palatino",
+                                    size: 20.0,
+                                    relativeTo: .body
+                                )
+                            )
+                            .foregroundColor(
+                                .teal
+                            )
+                        }
+                        .padding(
+                            .bottom,
+                            10
+                        )
+                        
+                        Text(
+                            "First Performer:"
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
+                            )
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
+                        Text(
+                            TradingCard.firstPerformer
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
+                            )
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
                     }
-                  
+                    
                     
                     VStack{
                         
                         
-                            Text("Notable Performer:")
-                            .font(
-                                .custom(
-                                    "Palatino",
-                                    size: 20.0,
-                                    relativeTo: .body
-                                )
+                        Text(
+                            "Notable Performer:"
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
                             )
-                            .foregroundColor(.teal)
-                            Text("Yuzuru Hanyu")
-                            .font(
-                                .custom(
-                                    "Palatino",
-                                    size: 20.0,
-                                    relativeTo: .body
-                                )
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
+                        Text(
+                            TradingCard.notablePerformer
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
                             )
-                            .foregroundColor(.teal)
-                        .padding(.bottom,10)
-                       
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
+                        .padding(
+                            .bottom,
+                            10
+                        )
                         
-                       
-                            Text("Notes:")
-                            .font(
-                                .custom(
-                                    "Palatino",
-                                    size: 20.0,
-                                    relativeTo: .body
-                                )
+                        
+                        
+                        Text(
+                            "Notes:"
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
                             )
-                            .foregroundColor(.teal)
-                            Text("Extended 4th pos.")
-                            .font(
-                                .custom(
-                                    "Palatino",
-                                    size: 20.0,
-                                    relativeTo: .body
-                                )
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
+                        Text(
+                            TradingCard.notes
+                        )
+                        .font(
+                            .custom(
+                                "Palatino",
+                                size: 20.0,
+                                relativeTo: .body
                             )
-                            .foregroundStyle(.teal)
+                        )
+                        .foregroundColor(
+                            .teal
+                        )
                         
                     }
                 }
@@ -189,7 +272,7 @@ struct ViewDetails: View {
     }
 }
 #Preview {
-    let beillmannSpin = TradingCard(
+    let biellmannSpin = TradingCard(
         imageName: "BeillmannSpin",
         elementName: "BeillmannSpin",
         type: "Upright Spin",
@@ -198,5 +281,7 @@ struct ViewDetails: View {
         notablePerformer: "Irina Slutskaya",
         notes: "Flexibility!!"
     )
-    return ViewDetails(tradingCard: beillmannSpin)
+    return ViewDetails(
+        tradingCard: biellmannSpin
+    )
 }
