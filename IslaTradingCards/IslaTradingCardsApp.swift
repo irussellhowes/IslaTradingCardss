a2a02bf14f9b009c113b4c973086ff694d885f12
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2024-11-18 22:54:45 -0500

    Instances and Trading card structure

--- IslaTradingCards/IslaTradingCardsApp.swift
+++ IslaTradingCards/IslaTradingCardsApp.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
-@main
-struct IslaTradingCardsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ViewDeatils()
-        }
-    }
+struct TradingCard {
+    let imageName: String
+    let elementName: String
+    let type: String
+    let baseValue: String
+    let firstPerformer: String
+    let notablePerformer: String
+    let notes: String
 }
+
+let laybackInaBauer = TradingCard(
+    imageName: "InaBauer",
+    elementName: "Layback Ina Bauer",
+    type: "MITF",
+    baseValue: "N/A",
+    firstPerformer: "Ina Bauer",
+    notablePerformer: "Yuzuru Hanyu",
+    notes: "Extended 4th pos."
+)
+
+let beillmannSpin = TradingCard(
+    imageName: "BeillmannSpin",
+    elementName: "BeillmannSpin",
+    type: "Upright Spin",
+    baseValue: "2.9",
+    firstPerformer:  "Denise Biellmann",
+    notablePerformer: "Irina Slutskaya",
+    notes: "Flexibility!!"
+)
